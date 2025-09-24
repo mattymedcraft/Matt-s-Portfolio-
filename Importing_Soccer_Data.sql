4697910841a63fae0dee037cb139f76eeea644de
mattymedcraft <mattymedcraft@gmail.com>
2025-09-24 16:27:30 -0400

    Update Importing_Soccer_Data.sql

--- Importing_Soccer_Data.sql
+++ Importing_Soccer_Data.sql
@@ -1,14 +1,10 @@
--- Enable local infile globally 
 SET GLOBAL local_infile = 1;
 
--- Use the correct schema 
 CREATE DATABASE IF NOT EXISTS kaggle_dataset;
 USE kaggle_dataset;
 
--- Drop table if it already exists
 DROP TABLE IF EXISTS kaggle_data;
 
--- Create the table
 CREATE TABLE kaggle_data (
     name VARCHAR(255),
     position VARCHAR(50),
@@ -22,7 +18,6 @@ CREATE TABLE kaggle_data (
     player_value VARCHAR(50)  -- 'values' is a reserved keyword, renamed to player_value
 );
 
--- Load the CSV file 
 LOAD DATA LOCAL INFILE 'C:/Users/matty/Downloads/Untitled spreadsheet - transfermarkt_players.csv'
 INTO TABLE kaggle_data
 CHARACTER SET utf8
@@ -30,3 +25,4 @@ FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS; 
+
