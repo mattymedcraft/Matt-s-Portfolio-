58198afe06a66da59cc3d30f3117aea3d7f728f7
mattymedcraft <mattymedcraft@gmail.com>
2025-09-24 16:18:49 -0400

    Add files via upload

---
+++ Create_Soccer_table.sql
@@ -0,0 +1,12 @@
+Create Table kaggle_data ( 
+	name varchar(255),
+    position varchar(50),
+    age INT, 
+    nation varchar(100), 
+    club varchar(100),
+    league varchar(100),
+    matches INT,
+    goals INT, 
+    points INT,
+    Value varchar(50)
+);
